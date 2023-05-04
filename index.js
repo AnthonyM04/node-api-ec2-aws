5b8fd27d0e3bbb463e8a5e0d9814ed6fba62866b
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-05-04 14:57:59 -0400

    commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import express from "express";
-import cors from 'cors'
+import express from "express"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.listen(4000)
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+    }
+)
+
+app.listen(4000,()=> {
+    console.log("API running on port 3000 😂")
+})
