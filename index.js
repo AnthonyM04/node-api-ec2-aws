631efb45062eb35f7e7c3904d3480e68e23796f2
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-05-04 16:09:27 -0400

    first commit

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+const dogs = [
+    {
+    name: "Paco",
+    breed: "Chihuahua",
+    age: 34
+    },
+    {
+        name: "Paco",
+        breed: "Chihuahua",
+        age: 34
+        },
+    {
+        name: "Buba",
+        breed: "Labrador",
+        age: 34
+        },
+    {
+        name: "Lucia",
+        breed: "Yorkie",
+        age: 34
+        },
+    {
+        name: "Paco",
+        breed: "Yorkie",
+        age: 34
+        },
+]
+
 app.get("/", (req, res) => {
     res.send("Hello World!")
     }
